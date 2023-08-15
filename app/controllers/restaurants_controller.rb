64a65eab2fffad004037c61b5aee59e1801f1b8c
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-15 09:41:49 +0100

    rest index done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
+    # get all the restaurant instances from db
+    # assign that to an instance variable to pass it onto the view
+    @restaurants = Restaurant.all
   end
 end
