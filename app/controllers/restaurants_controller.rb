779def3f843c79383e98ec818d72d01cdd863e4c
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-15 11:28:39 +0100

    lec done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,13 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show edit update destroy]
+
   def index
     # get all the restaurant instances from db
     # assign that to an instance variable to pass it onto the view
     @restaurants = Restaurant.all
   end
 
-  def show
-    # get the id in the URL
-    id = params[:id]
-    # get the instance of the restaurant with the id
-    # assign the instance to an instance variable
-    @restaurant = Restaurant.find(id)
-  end
+  def show; end
 
   def new
     @restaurant = Restaurant.new
@@ -19,16 +15,42 @@ class RestaurantsController < ApplicationController
 
   def create
     strong_params = restaurant_params
-    restaurant = Restaurant.new(strong_params)
+    @restaurant = Restaurant.new(strong_params)
     # save the instance to the DB
-    restaurant.save
+    @restaurant.save
 
-    redirect_to restaurant_path(restaurant.id)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    # find the instance of the restaurant we want to edit
+    # assign that to an instance variable and pass it onto view
+  end
+
+  def update
+    # we give the new params to the insatnce
+    # we save the changes to the DB
+    @restaurant.update(restaurant_params)
+    # redirect the user to the show page
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    # destroy method onto that instance
+    @restaurant.destroy
+
+    # redirect the user to the index page
+    redirect_to restaurants_path, status: :see_other
   end
 
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :rating, :address)
+  end
+
+  def set_restaurant
+    # find the instance with the ID
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new', to: 'restaurants#new'
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-  post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+  resources :restaurants
 end
